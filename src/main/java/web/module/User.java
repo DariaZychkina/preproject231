9f564dc6d5167da1d6900b32ca5ee440a83872eb
DariaZychkina <99396496+DariaZychkina@users.noreply.github.com>
2022-12-08 10:27:04 +0300

    Update User.java

--- src/main/java/web/module/User.java
+++ src/main/java/web/module/User.java
@@ -51,5 +51,18 @@ public class User {
     public void setPersonWatchedTheSupernatural(boolean watchedTheSupernatural) {
         this.personWatchedTheSupernatural = watchedTheSupernatural;
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && personWatchedTheSupernatural == user.personWatchedTheSupernatural && Objects.equals(name, user.name) && Objects.equals(surname, user.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, personWatchedTheSupernatural);
+    }
 
 }
